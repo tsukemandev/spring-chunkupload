6de4645204904ef234170e73f9c14738db15f54b
tsukemendog <mokaim@outlook.com>
2023-12-09 23:09:00 +0900

    웹소켓 설정

---
+++ src/main/java/com/tsukemendog/openbankinglink/websocket/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.tsukemendog.openbankinglink.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+//simp 패키지 문서
+//https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/messaging/simp/config/package-summary.html
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/gs-guide-websocket");
+    }
+}
