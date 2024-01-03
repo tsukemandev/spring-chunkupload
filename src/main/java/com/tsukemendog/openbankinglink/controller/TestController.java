dd50fcddcb12a1020be9e98aaea925d880e40fe6
tsukemendog <mokaim@outlook.com>
2024-01-03 23:10:50 +0900

    AWS SDK 의존성 추가 및 S3 객체 업로드 테스트완료

--- src/main/java/com/tsukemendog/openbankinglink/controller/TestController.java
+++ src/main/java/com/tsukemendog/openbankinglink/controller/TestController.java
@@ -27,6 +27,19 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.*;
 
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.transfer.s3.S3TransferManager;
+import software.amazon.awssdk.transfer.s3.model.CompletedFileUpload;
+import software.amazon.awssdk.transfer.s3.model.FileUpload;
+import software.amazon.awssdk.transfer.s3.model.UploadFileRequest;
+import software.amazon.awssdk.transfer.s3.progress.LoggingTransferListener;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
 @RestController
 public class TestController {
 
@@ -191,5 +204,46 @@ public class TestController {
 
         return count;
     }
+
+
+
+    @GetMapping("/s3")
+    public ResponseEntity<String> s3UploadTest() {
+
+        uploadFile(getS3TransferManager(), "pizzadog-video-s3-bucket", UUID.randomUUID().toString(), "C:\\Users\\mokai\\OneDrive\\Desktop\\블로그이미지\\image.png");
+
+        return ResponseEntity.ok("ok");
+    }
+
+
+
+
+    public String uploadFile(S3TransferManager transferManager, String bucketName,
+                             String key, String filePath) {
+        UploadFileRequest uploadFileRequest =
+            UploadFileRequest.builder()
+                .putObjectRequest(b -> b.bucket(bucketName).key(key))
+                .addTransferListener(LoggingTransferListener.create())
+                .source(Paths.get(filePath))
+                .build();
+
+        FileUpload fileUpload = transferManager.uploadFile(uploadFileRequest);
+
+        CompletedFileUpload uploadResult = fileUpload.completionFuture().join();
+        return uploadResult.response().eTag();
+    }
+
+
+    public S3TransferManager getS3TransferManager() {
+        S3AsyncClient s3AsyncClient = S3AsyncClient.builder()
+                .region(Region.AP_NORTHEAST_2) // AWS 리전 설정
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+
+        return S3TransferManager.builder()
+                .s3Client(s3AsyncClient)
+                .build();
+    }
+
 
 }
